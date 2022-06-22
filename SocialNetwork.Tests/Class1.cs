7f3483fdb5204919f87a447322b03adde9db909f
halfbrikbarry@gmail.com <halfbrikbarry@gmail.com>
2022-06-22 21:00:37 +0300

    Добавление теста

---
+++ SocialNetwork.Tests/Class1.cs
@@ -0,0 +1,37 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entity;
+using SocialNetwork.DAL.Repositories;
+using NUnit.Framework;
+using SocialNetwork;
+using System;
+using SocialNetwork.BLL.Services;
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestForCreatingUser()
+        {
+            UserRepository testUser = new UserRepository();
+
+            var TestUserData = new UserEntity()
+            {
+                firstname = "a",
+                lastname = "a",
+                email = "a@gmil.com",
+                password = "00000000",
+            };
+
+            testUser.Create(TestUserData);            
+
+
+
+            Assert.True(testUser.Create(TestUserData) != 0);
+
+        }
+
+
+    }
+}
